16c1aeb52496874788fc48fd44c0fa853b6ff443
KelseyKrehbiel <krehbielk@gmail.com>
2020-11-06 00:17:00 -0700

    fixed octokit import. requirements met

--- scripts.js
+++ scripts.js
@@ -1,17 +1,37 @@
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 const octokit = new Octokit();
 //var client = new GitHubClient(new ProductHeaderValue("Find-Github-Repos"));
 
 
 //response.name to get name of the repos
 async function getRepos(username) {
+    console.log("getting repos");
     await octokit.request('GET /users/{username}/repos', {
         username: username
-    }).then(console.log(response));
+    }).then(userData => {
+        console.log("i got something");
+        //console.log(userData.data);
+        if (userData.status === 200){
+            console.log("passed check")
+            //console.log(userData.data);
+            displayRepos(userData.data);
+        }
+        else{
+            console.log(userData.statusText);
+        }
+    });
 }
 
-function displayRepos(repoList){
-    console.log(repoList);
+function displayRepos(repoData){
+    console.log(repoData);
+    $('.repos').remove();
+    console.log(repoData.length)
+    let repoList = ""
+    for(let r = 0; r < repoData.length; r++){
+        repoList += `<p class="repos"><a href="${repoData[r].html_url}">${repoData[r].name}</a></p>`
+    }
+    $('main').append(repoList);
 }
 
 
